8c30bd4f8ddbff695faea12fce4495944af2bd38
KellyReisLee <kristinaalvares28@gmail.com>
2024-01-18 14:01:37 +0000

    Updating the Button Comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,15 @@
 import { useState } from 'react'
 import Wrapper from './components/Wrapper/Wrapper'
-import Button from './components/Button/Button';
 import Title from './components/Title/Title';
 import ContainerContent from './components/ContainerContent/ContainerContent';
 import './App.css';
-import VolumeUpRoundedIcon from '@mui/icons-material/VolumeUpRounded';
-import VolumeOffRoundedIcon from '@mui/icons-material/VolumeOffRounded';
-import MusicNoteRoundedIcon from '@mui/icons-material/MusicNoteRounded';
-import MusicOffRoundedIcon from '@mui/icons-material/MusicOffRounded';
-import AllOutRoundedIcon from '@mui/icons-material/AllOutRounded';
-import QuestionMarkRoundedIcon from '@mui/icons-material/QuestionMarkRounded';
-import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
+import CustomButtons from './components/Button/CustomButtons'
 
 
 
 
 function App() {
   const [sound, setSound] = useState(true);
-
-  function handleSound() {
-    console.log('Sound');
-  }
-  function handleMusic() {
-    console.log('Music');
-  }
-  function handleExpand() {
-    console.log('Expand');
-  }
-  function handleHelp() {
-    console.log('Help');
-  }
-
-  function handlePlay() {
-    console.log('Play');
-  }
-  const buttons = [
-    {
-      id: 1,
-      icon: sound ? < VolumeUpRoundedIcon sx={{ fontSize: 60 }} /> : <VolumeOffRoundedIcon sx={{ fontSize: 60 }} />,
-      function: () => handleSound(),
-    },
-    {
-      id: 2,
-      icon: sound ? < MusicNoteRoundedIcon sx={{ fontSize: 60 }} /> : <MusicOffRoundedIcon sx={{ fontSize: 60 }} />,
-      function: () => handleMusic()
-    },
-    {
-      id: 3,
-      icon: <AllOutRoundedIcon sx={{ fontSize: 60 }} />,
-      function: () => handleExpand()
-    },
-    {
-      id: 4,
-      icon: <QuestionMarkRoundedIcon sx={{ fontSize: 60 }} />,
-      function: () => handleHelp()
-    },
-    {
-      id: 5,
-      icon: <PlayArrowRoundedIcon sx={{ fontSize: 60 }} />,
-      function: () => handlePlay()
-    }
-  ]
 
 
 
@@ -72,12 +21,7 @@ function App() {
             <Title text='Memory Game' />
             <h3>Monsters</h3>
           </div>
-          <div className='flex-icon-btns'>
-            {buttons.map((btn) => (
-              <Button key={btn.id} onChangeAction={btn.function}>{btn.icon}</Button>
-            ))}
-
-          </div>
+          <CustomButtons sound={sound} />
         </ContainerContent>
       </Wrapper>
     </section>
---
+++ src/components/Button/CustomButtons.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import VolumeUpRoundedIcon from '@mui/icons-material/VolumeUpRounded';
+import VolumeOffRoundedIcon from '@mui/icons-material/VolumeOffRounded';
+import MusicNoteRoundedIcon from '@mui/icons-material/MusicNoteRounded';
+import MusicOffRoundedIcon from '@mui/icons-material/MusicOffRounded';
+import AllOutRoundedIcon from '@mui/icons-material/AllOutRounded';
+import QuestionMarkRoundedIcon from '@mui/icons-material/QuestionMarkRounded';
+import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
+import './CustomButtons.css'
+
+export default function CustomButtons({ sound }) {
+  // replace this with your state or prop
+
+  const BUTTONS = [
+    {
+      id: 1,
+      icon: sound ? <VolumeUpRoundedIcon sx={{ fontSize: 60 }} /> : <VolumeOffRoundedIcon sx={{ fontSize: 60 }} />,
+      function: () => handleSound()
+    },
+    {
+      id: 2,
+      icon: sound ? <MusicNoteRoundedIcon sx={{ fontSize: 60 }} /> : <MusicOffRoundedIcon sx={{ fontSize: 60 }} />,
+      function: () => handleMusic()
+
+    },
+    {
+      id: 3,
+      icon: <AllOutRoundedIcon sx={{ fontSize: 60 }} />,
+      function: () => handleExpand()
+    },
+    {
+      id: 4,
+      icon: <QuestionMarkRoundedIcon sx={{ fontSize: 60 }} />,
+      function: () => handleHelp()
+    },
+    {
+      id: 5,
+      icon: <PlayArrowRoundedIcon sx={{ fontSize: 60 }} />,
+      function: () => handlePlay()
+    },
+  ];
+  function handleSound() {
+
+    console.log('Sound');
+  }
+
+  function handleMusic() {
+    console.log('Music');
+  }
+
+  function handleExpand() {
+    console.log('Expand');
+  }
+
+  function handleHelp() {
+    console.log('Help');
+  }
+
+  function handlePlay() {
+    console.log('Play');
+  }
+
+  return (
+    <div className='flex-icon-btns'>
+      {BUTTONS.map(button => (
+        <button key={button.id} className='btn-icons' onClick={button.function}>
+          {button.icon}</button>
+
+      ))}
+    </div>
+  );
+}
+
