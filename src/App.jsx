c3f88c4717f63e2db2a5b99ceddb93f969170ee5
KellyReisLee <kristinaalvares28@gmail.com>
2024-01-18 10:59:27 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <section className='app'>
+      <h1>Hello!</h1>
+    </section>
+  )
+}
+
+export default App
